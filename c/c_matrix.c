8d2484afa67238c95402a25002a2c18bdf9b0f5d
Alexandre <alex.paivasilva@poli.ufrj.br>
2019-04-23 23:21:22 -0300

    Convertido código C e tempos medidos em C para multiplicação de Matriz X Vetor

--- c/c_matrix.c
+++ c/c_matrix.c
@@ -9,54 +9,46 @@ int main(int argc, char *argv[]){
     char *size = argv[1];
     int N = atoi(size);
     printf("Size of Matrix: %d\n\n", N);
-    float m1[N][N],m2[N][N]; // Declares 2 matrices of size N
-    float ** m_res; // And a result matrix.
+    float m1[N][N]; // Declares a matrix of size N
+    float m2[N]; // Declares a vector of size N
+    float * m_res; // And a result matrix.
     int i, j, k; // Iterators
     clock_t t; // Declares a clock
 
-    // Initialize Matrix 1
+    // Initialize Matrix
     for(i=0; i<N; ++i){
         for(j=0; j<N; ++j){
             m1[i][j] = i*i*1.7453+j*3.674528;
         }
     }
 
-    // Initialize Matrix 2
+    // Initialize Vector
     for(i=0; i<N; ++i){
-        for(j=0; j<N; ++j){
-            m2[i][j] =  i*j*1.3569875*6.76512;
-        }
+        m2[i] =  i*1.3569875*6.76512;
     }
 
-    m_res = (float **)calloc(N, sizeof(float *)); // Allocate an initial array for matrix
-    for (int i = 0; i < N; i++)
-        m_res[i] = (float *)calloc(N, sizeof(float)); // Allocate the array for each array (now, it's N by N)
+    m_res = (float *)calloc(N, sizeof(float)); // Allocate an initial array for matrix
 
     t = clock(); // Starts the time
     float tmp;
     for (i = 0; i < N; i++) {
+        tmp = 0.0;
         for (j = 0; j < N; j++) {
-            tmp = 0.0;
-            for (k = 0; k < N; k++)
-                tmp += m1[i][k] * m2[k][j]; // Computes the product
-            m_res[i][j] = tmp; // Assign value to the matrix
+            tmp += m1[i][j] * m2[j]; // Computes the product
         }
+        m_res[i] = tmp; // Assign value to the matrix
     }
     double exec_time = ((double)t)/CLOCKS_PER_SEC; // Finishes the time
     printf("Time to Row = %f seconds.\n", exec_time);
     
     t = clock(); // Starts the time
-    tmp = 0.0;
     for (j = 0; j < N; j++) {
+        tmp = 0.0;
         for (i = 0; i < N; i++) {
-            tmp = 0.0;
-            for (k = 0; k < N; k++)
-                tmp += m1[i][k] * m2[k][j]; // Computes the product
-            m_res[i][j] = tmp; // Assign value to the matrix
+            tmp += m1[i][j] * m2[j]; // Computes the product
         }
-    }
+        m_res[i] = tmp; // Assign value to the matrix
+    }    
     exec_time = ((double)t)/CLOCKS_PER_SEC; // Finishes the time
     printf("Time to Column = %f seconds.\n", exec_time);
-
-    printf("%f %f %f\n",m_res[0][0],m_res[0][1],m_res[0][2]);
 }
