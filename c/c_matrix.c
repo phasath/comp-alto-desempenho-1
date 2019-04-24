3774162edc2ef41e5d07c0e79d4262186e221b7c
phasath <sathler93@gmail.com>
2019-04-23 23:19:43 -0300

    Correção do algoritmo em C para matriz * vetor

--- c/c_matrix.c
+++ c/c_matrix.c
@@ -9,8 +9,8 @@ int main(int argc, char *argv[]){
     char *size = argv[1];
     int N = atoi(size);
     printf("Size of Matrix: %d\n\n", N);
-    float m1[N][N],m2[N][N]; // Declares 2 matrices of size N
-    float ** m_res; // And a result matrix.
+    float m1[N][N],v1[N]; // Declares 1 matrix of size N and a vector of size N
+    float * v_res; // And a result matrix.
     int i, j, k; // Iterators
     clock_t t; // Declares a clock
 
@@ -23,24 +23,20 @@ int main(int argc, char *argv[]){
 
     // Initialize Matrix 2
     for(i=0; i<N; ++i){
-        for(j=0; j<N; ++j){
-            m2[i][j] =  i*j*1.3569875*6.76512;
-        }
+            v1[i] =  i*i*1.3569875*6.76512;
     }
 
-    m_res = (float **)calloc(N, sizeof(float *)); // Allocate an initial array for matrix
-    for (int i = 0; i < N; i++)
-        m_res[i] = (float *)calloc(N, sizeof(float)); // Allocate the array for each array (now, it's N by N)
+    v_res = (float *)calloc(N, sizeof(float)); // Allocate an initial array for matrix
+    
 
     t = clock(); // Starts the time
     float tmp;
     for (i = 0; i < N; i++) {
+        tmp = 0.0;
         for (j = 0; j < N; j++) {
-            tmp = 0.0;
-            for (k = 0; k < N; k++)
-                tmp += m1[i][k] * m2[k][j]; // Computes the product
-            m_res[i][j] = tmp; // Assign value to the matrix
+            tmp += m1[i][j] * v1[j]; // Computes the product
         }
+        v_res[i] = tmp; // Assign value to the matrix
     }
     double exec_time = ((double)t)/CLOCKS_PER_SEC; // Finishes the time
     printf("Time to Row = %f seconds.\n", exec_time);
@@ -48,15 +44,14 @@ int main(int argc, char *argv[]){
     t = clock(); // Starts the time
     tmp = 0.0;
     for (j = 0; j < N; j++) {
+        tmp = 0.0;
         for (i = 0; i < N; i++) {
-            tmp = 0.0;
-            for (k = 0; k < N; k++)
-                tmp += m1[i][k] * m2[k][j]; // Computes the product
-            m_res[i][j] = tmp; // Assign value to the matrix
+            tmp += m1[i][j] * v1[j]; // Computes the product
         }
+        v_res[i] = tmp; // Assign value to the matrix
     }
     exec_time = ((double)t)/CLOCKS_PER_SEC; // Finishes the time
     printf("Time to Column = %f seconds.\n", exec_time);
 
-    printf("%f %f %f\n",m_res[0][0],m_res[0][1],m_res[0][2]);
+    printf("%f %f %f\n",v_res[0],v_res[1],v_res[2]);
 }
